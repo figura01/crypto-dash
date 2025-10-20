6b44bc7bd8c8c58fd18ba8613fcecd69a0915ede
laurentvuillaume <figura.graphik@gmail.com>
2025-10-20 17:52:48 +0200

    add types CoinDetailsProps

--- types/index.tsx
+++ types/index.tsx
@@ -8,3 +8,56 @@ export type FetchedCoin = {
   price_percentage_24h: number;
   market_cap: number;
 };
+
+export type CoinDetailProps = {
+  id: string;
+  symbol: string;
+  name: string;
+  description: {
+    en: string;
+  };
+  categories: string[];
+  image: {
+    large: string;
+    small: string;
+    thumb: string;
+  };
+  market_data: {
+    current_price: {
+      usd: number;
+    };
+    market_cap: {
+      usd: number;
+    };
+    high_24h: {
+      usd: number;
+    };
+    low_24h: {
+      usd: number;
+    };
+    price_change_24h: number;
+    price_change_percentage_24h: number;
+    circulating_supply: number;
+    total_supply: number | null;
+  };
+  market_cap_rank: number;
+  links: {
+    homepage: string[];
+    blockchain_site: string[];
+  };
+};
+
+export type ChartDataProps = {
+  datasets: {
+    label: string;
+    data: {
+      x: number;
+      y: number;
+    }[];
+    fill: boolean;
+    borderColor: string;
+    backgroundColor: string;
+    pointRadius: number;
+    tension: number;
+  }[];
+};
