ee9bf5cdcb6e7890f1c2d800cf1f316f1b826576
laurentvuillaume <figura.graphik@gmail.com>
2025-10-20 12:26:26 +0200

    add link to details page, on coin-card component

--- src/components/coin-card.tsx
+++ src/components/coin-card.tsx
@@ -1,25 +1,28 @@
 import type { FetchedCoin } from "../../types";
+import { Link } from "react-router";
 
 const CoinCard = ({ coin }: { coin: FetchedCoin }) => {
   return (
-    <div className="coin-card" key={coin.id}>
-      <div className="coin-header">
-        <img src={coin.image} alt={coin.name} className="coin-image" />
-        <div>
-          <h2>{coin.name}</h2>
-          <p className="symnol">{coin.symbol.toUpperCase()}</p>
+    <Link to={`/coin/${coin.id}`}>
+      <div className="coin-card" key={coin.id}>
+        <div className="coin-header">
+          <img src={coin.image} alt={coin.name} className="coin-image" />
+          <div>
+            <h2>{coin.name}</h2>
+            <p className="symnol">{coin.symbol.toUpperCase()}</p>
+          </div>
+          <p>Price: ${coin.current_price.toLocaleString()}</p>
         </div>
-        <p>Price: ${coin.current_price.toLocaleString()}</p>
+        <p
+          className={
+            coin.price_change_percentage_24h >= 0 ? "positive" : "negative"
+          }
+        >
+          {coin.price_change_percentage_24h.toFixed(2)} %
+        </p>
+        <p>Market Cap: {coin.market_cap.toLocaleString()}</p>
       </div>
-      <p
-        className={
-          coin.price_change_percentage_24h >= 0 ? "positive" : "negative"
-        }
-      >
-        {coin.price_change_percentage_24h.toFixed(2)} %
-      </p>
-      <p>Market Cap: {coin.market_cap.toLocaleString()}</p>
-    </div>
+    </Link>
   );
 };
 
